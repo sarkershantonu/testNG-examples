782f9568fbc191bf541b0633d46cb97ea3134f4f
shantonu sarker <sarker.shantonu@gmail.com>
2022-07-27 00:02:24 -0500

    context example

--- src/test/java/org/automation/tests/context/CalculatorTest.java
+++ src/test/java/org/automation/tests/context/CalculatorTest.java
@@ -1,5 +1,25 @@
 package org.automation.tests.context;
 
-public class CalculatorTest {
+import org.testng.Assert;
+import org.testng.annotations.Test;
 
+import static org.testng.Assert.assertEquals;
+
+public class CalculatorTest extends BaseTestWithContext{
+    @Test
+    public void testAdditionFail() {
+        assertEquals(35.0, aCalculator.add(10.0, 15.0), 0.01);
+
+    }
+
+    @Test
+    public void testAddition() {
+        assertEquals(25.0, aCalculator.add(10.0, 15.0), 0.01);
+
+    }
+    @Test
+    public void testAdditionForText() {
+        assertEquals(25.0, aCalculator.add(10.0, 15.0), 0.01);
+
+    }
 }
