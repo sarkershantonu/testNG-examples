ecc35a6c3c073ef465d04b3ce0df9f163ac194c2
shantonu sarker <sarker.shantonu@gmail.com>
2022-07-27 00:28:39 -0500

    context example

--- src/test/java/org/automation/tests/context/CalculatorTest.java
+++ src/test/java/org/automation/tests/context/CalculatorTest.java
@@ -6,17 +6,17 @@ import org.testng.annotations.Test;
 import static org.testng.Assert.assertEquals;
 
 public class CalculatorTest extends BaseTestWithContext{
-    @Test
+    @Test(priority = 1)
     public void testAdditionFail(ITestContext context) {
         context.setAttribute("env","DEV");
         assertEquals(35.0, aCalculator.add(10.0, 15.0), 0.01);
 
     }
 
-    @Test
+    @Test(priority = 3)
     public void testAddition(ITestContext context) {
         assertEquals(25.0, aCalculator.add(10.0, 15.0), 0.01);
-        System.out.println(context.getAttribute("env"));
+        System.out.println(context.getAttribute("env"));//you should see printout of DEV
 
     }
     @Test
