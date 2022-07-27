e6a90edfc5d6a2e99c94af6c3b1e3e600c19ee34
shantonu sarker <sarker.shantonu@gmail.com>
2022-07-27 00:27:51 -0500

    context example

--- src/test/java/org/automation/tests/context/BaseTestWithContext.java
+++ src/test/java/org/automation/tests/context/BaseTestWithContext.java
@@ -1,7 +1,12 @@
 package org.automation.tests.context;
 
 import org.automation.app.calculator.Calculator;
+import org.testng.annotations.BeforeTest;
 
 public abstract class BaseTestWithContext {
     protected Calculator aCalculator;
+    @BeforeTest
+    public void initCalculator(){
+        aCalculator = new Calculator();
+    }
 }
