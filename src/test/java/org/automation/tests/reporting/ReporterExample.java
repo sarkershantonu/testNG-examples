4ba89e5054c8c0a3c98ed89639351ad9fb370815
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-01 00:01:27 -0500

    testNG report example withLog4j

--- src/test/java/org/automation/tests/reporting/ReporterExample.java
+++ src/test/java/org/automation/tests/reporting/ReporterExample.java
@@ -1,6 +1,7 @@
 package org.automation.tests.reporting;
 
 import org.automation.tests.context.BaseTestWithContext;
+import org.automation.tests.core.BaseTest;
 import org.testng.ITestContext;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
@@ -13,7 +14,8 @@ public class ReporterExample extends BaseTestWithContext {
     @Test(priority = 0)
     public void testAddition(ITestContext context, Method m) {
         Reporter.clear();
-        Reporter.log("Running "+m.getName());
+        Reporter.log("Running " + m.getName());
         assertEquals(25.0, aCalculator.add(10.0, 15.0), 0.01);
-            }
+
+    }
 }
