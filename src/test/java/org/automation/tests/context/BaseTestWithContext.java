ff6774d34ef6a0e0f274fc5fb37d9a67084dfc31
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-02 00:41:59 -0500

    custom reporter : log

--- src/test/java/org/automation/tests/context/BaseTestWithContext.java
+++ src/test/java/org/automation/tests/context/BaseTestWithContext.java
@@ -1,9 +1,12 @@
 package org.automation.tests.context;
 
 import org.automation.app.calculator.Calculator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.annotations.BeforeTest;
 
 public abstract class BaseTestWithContext {
+    protected final static Logger log = LoggerFactory.getLogger(BaseTestWithContext.class);
     protected Calculator aCalculator;
     @BeforeTest
     public void initCalculator(){
