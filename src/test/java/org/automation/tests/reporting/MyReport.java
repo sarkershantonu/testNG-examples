c4f727d6d125bc29e6203d52b8fa6698b06bbe63
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-02 00:40:56 -0500

    custom reporter

---
+++ src/test/java/org/automation/tests/reporting/MyReport.java
@@ -0,0 +1,45 @@
+package org.automation.tests.reporting;
+
+import org.slf4j.Logger;
+import org.testng.*;
+import org.testng.xml.XmlSuite;
+
+import java.util.List;
+import java.util.Map;
+
+public class MyReport implements IReporter {
+    private final Logger log;
+
+    public MyReport(Logger log) {
+        this.log = log;
+    }
+
+    @Override
+    public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
+        for(ISuite aSuite: suites){
+            final String suiteName = aSuite.getName();
+            Map<String, ISuiteResult> resultMap = aSuite.getResults();
+            for(ISuiteResult aResult : resultMap.values()){
+                ITestContext context = aResult.getTestContext();
+                log.info(passText(suiteName,context));
+                log.info(failedText(suiteName,context));
+                log.info(skippedText(suiteName,context));
+            }
+        }
+        
+        IReporter.super.generateReport(xmlSuites, suites, outputDirectory);
+    }
+
+    private String skippedText(String suiteName, ITestContext context) {
+        return  suiteName + " PASSED , total tests "+ context.getSkippedTests().getAllResults().size();
+    }
+
+    private String failedText(String suiteName, ITestContext context) {
+        return suiteName + " PASSED , total tests "+ context.getFailedTests().getAllResults().size();
+    }
+
+    private String passText(String suiteName, ITestContext context) {
+        return  suiteName + " PASSED , total tests "+ context.getPassedTests().getAllResults().size();
+    }
+
+}
