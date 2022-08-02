10292e009d1bc3afe60632ca39e8ad680bff8491
shantonu sarker <sarker.shantonu@gmail.com>
2022-08-02 00:53:35 -0500

    custom reporter : complete

--- src/test/java/org/automation/tests/reporting/MyReport.java
+++ src/test/java/org/automation/tests/reporting/MyReport.java
@@ -1,6 +1,7 @@
 package org.automation.tests.reporting;
 
 import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.testng.*;
 import org.testng.xml.XmlSuite;
 
@@ -8,11 +9,7 @@ import java.util.List;
 import java.util.Map;
 
 public class MyReport implements IReporter {
-    private final Logger log;
-
-    public MyReport(Logger log) {
-        this.log = log;
-    }
+    private final Logger log = LoggerFactory.getLogger(MyReport.class);
 
     @Override
     public void generateReport(List<XmlSuite> xmlSuites, List<ISuite> suites, String outputDirectory) {
@@ -31,11 +28,11 @@ public class MyReport implements IReporter {
     }
 
     private String skippedText(String suiteName, ITestContext context) {
-        return  suiteName + " PASSED , total tests "+ context.getSkippedTests().getAllResults().size();
+        return  suiteName + " Skipped , total tests "+ context.getSkippedTests().getAllResults().size();
     }
 
     private String failedText(String suiteName, ITestContext context) {
-        return suiteName + " PASSED , total tests "+ context.getFailedTests().getAllResults().size();
+        return suiteName + " failed , total tests "+ context.getFailedTests().getAllResults().size();
     }
 
     private String passText(String suiteName, ITestContext context) {
